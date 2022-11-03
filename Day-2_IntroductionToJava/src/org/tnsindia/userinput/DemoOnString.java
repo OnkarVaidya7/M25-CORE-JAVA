407439e830038ec1c8b963f28d793df4d01eefe8
OnKaR <OnKaR@ASUS>
2022-11-03 14:15:01 +0530

    First Commit

---
+++ Day-2_IntroductionToJava/src/org/tnsindia/userinput/DemoOnString.java
@@ -0,0 +1,18 @@
+package org.tnsindia.userinput;
+import java.util.Scanner;
+
+public class DemoOnString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//For user input value	
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the String: ");
+		String x=s.nextLine();
+		System.out.println("The result is : ");
+		System.out.println(x);	
+		s.close();
+
+	}
+
+}
