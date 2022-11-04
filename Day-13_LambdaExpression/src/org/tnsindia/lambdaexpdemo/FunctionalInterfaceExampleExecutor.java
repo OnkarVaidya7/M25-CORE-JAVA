d00659fee173873b9aa5966f99a3d14270e989f2
OnKaR <OnKaR@ASUS>
2022-11-04 22:08:44 +0530

    First Commit

---
+++ Day-13_LambdaExpression/src/org/tnsindia/lambdaexpdemo/FunctionalInterfaceExampleExecutor.java
@@ -0,0 +1,19 @@
+package org.tnsindia.lambdaexpdemo;
+
+public class FunctionalInterfaceExampleExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FunctionalInterfaceExample obj=(int a,int b)->
+		{
+			int result=(a>b)?a:b;
+			
+			//System.out.println("The maximum number is:" +result);
+			return result;
+			
+		};
+		System.out.println("The maximum number is:" +obj.max(34, 67));
+
+	}
+
+}
---
+++ Day-13_LambdaExpression/src/org/tnsindia/lambdaexpdemo/FunctionalInterfaceExecutor.java
@@ -0,0 +1,16 @@
+package org.tnsindia.lambdaexpdemo;
+
+public class FunctionalInterfaceExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//lambda expression
+				FunctionalInterfaceDemo obj=(String str)->
+				{
+					return str;
+				};
+				System.out.println(obj.display("Welcome to the concept of Lambda Expression"));
+
+	}
+
+}
