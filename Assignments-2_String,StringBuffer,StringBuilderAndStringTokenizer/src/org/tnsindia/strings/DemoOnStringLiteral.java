7797abff531a0d62fae471dbbd16490a516c1d60
OnKaR <OnKaR@ASUS>
2022-11-04 20:13:42 +0530

    First Commit

---
+++ Assignments-2_String,StringBuffer,StringBuilderAndStringTokenizer/src/org/tnsindia/strings/DemoOnStringLiteral.java
@@ -0,0 +1,17 @@
+package org.tnsindia.strings;
+import java.util.Scanner;
+public class DemoOnStringLiteral {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Vrushali Varankar";
+		System.out.println(str);
+		System.out.println("Enter any name: ");
+		Scanner s=new Scanner(System.in);
+		String str1=s.nextLine();
+		System.out.println(str1);
+		s.close();
+
+	}
+
+}
---
+++ Assignments-2_String,StringBuffer,StringBuilderAndStringTokenizer/src/org/tnsindia/strings/Example1.java
@@ -0,0 +1,16 @@
+package org.tnsindia.strings;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String str="Hello";
+				String str="Hello";
+				String str1=new String("Hello");
+				String str2=new String("Hello");
+				System.out.println(str==str2);
+				System.out.println(str1.equals(str2));//output will be true
+
+	}
+
+}
