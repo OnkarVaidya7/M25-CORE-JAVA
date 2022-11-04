7fb68de948660a4d6e827c257f1a4ad8595b8aa6
OnKaR <OnKaR@ASUS>
2022-11-04 20:29:03 +0530

    First Commit

---
+++ Assignment-3_Array/src/org/tnsindia/arraydemo/TwoDimensionalArray.java
@@ -0,0 +1,16 @@
+package org.tnsindia.arraydemo;
+
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][]= {{11,3,5},{8,65,87}};
+		System.out.println(arr[0][2]);
+		System.out.println(arr[1][1]);
+
+	}
+
+}
+
+
+//two dimensional array=>arrays of array
---
+++ Assignment-3_Array/src/org/tnsindia/arraydemo/Varargs.java
@@ -0,0 +1,24 @@
+package org.tnsindia.arraydemo;
+
+public class Varargs {
+	
+	//variable argument ...(ellipse)=>
+			static void print(int a,int b, String... s)
+			{
+				System.out.println(a+","+b);
+				//TO PRINT VARARGS
+				for(int i=0;i<s.length;i++)
+				{
+					System.out.println(s[i]+"\t");
+				}
+			}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//fun call
+				print(13,65,"Python","Java","C++");
+
+	}
+
+}
